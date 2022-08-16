024ce9aa8da4df8bf5d2d0e0015b564bf55fe7c9
KarimLeeKarim <kerimlikerim24@gmail.com>
2022-08-16 16:11:21 +0400

    deploy

--- src/routes/RouteProvider.js
+++ src/routes/RouteProvider.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 export const RouteProvider = ({ children }) => {
     return <Router>{children}</Router>;
